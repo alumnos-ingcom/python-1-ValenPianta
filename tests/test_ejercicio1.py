03444672c43bb966f150e73b605406d94bcd5a58
Valentín Piantanida <valenpiantanida@gmail.com>
2022-06-05 18:07:36 -0300

    Agrego casos de prueba para valores negativos test_ejercicio1.py

--- tests/test_ejercicio1.py
+++ tests/test_ejercicio1.py
@@ -10,7 +10,7 @@ from src.ejercicio1 import convertir_a_fahrrenheit, convertir_a_centigrados
 Este es el test correspondiente al archivo 'ejercicio1.py'
 """
 
-def test_convertir_a_fahrrenheit():
+def test_convertir_a_fahrrenheit_positivos():
     """
     Este test evalua la funcion convertir_a_fahrrenheit() ingresando un valor y
     revisando que el resultado sea el numero esperado y de tipo Float
@@ -19,8 +19,18 @@ def test_convertir_a_fahrrenheit():
     resultado = convertir_a_fahrrenheit(numero)
     assert isinstance(resultado, float), "El resultado debe ser un numero tipo Float"
     assert resultado == 87.8, "El resultado no es el esperado"
+    
+def test_convertir_a_fahrrenheit_negativos():
+    """
+    Este test evalua la funcion convertir_a_fahrrenheit() ingresando un valor y
+    revisando que el resultado sea el numero esperado y de tipo Float
+    """
+    numero = -10
+    resultado = convertir_a_fahrrenheit(numero)
+    assert isinstance(resultado, float), "El resultado debe ser un numero tipo Float"
+    assert resultado == 14, "El resultado no es el esperado"
 
-def test_convertir_a_centigrados():
+def test_convertir_a_centigrados_positivos():
     """
     Este test evalua la funcion convertir_a_centigrados() ingresando un valor y
     revisando que el resultado sea el numero esperado y de tipo Float
@@ -29,3 +39,13 @@ def test_convertir_a_centigrados():
     resultado = convertir_a_centigrados(numero)
     assert isinstance(resultado, float), "El resultado debe ser un numero tipo Float"
     assert resultado == 31, "El resultado no es el esperado"
+    
+def test_convertir_a_centigrados_negativos():
+    """
+    Este test evalua la funcion convertir_a_centigrados() ingresando un valor y
+    revisando que el resultado sea el numero esperado y de tipo Float
+    """
+    numero = -5
+    resultado = convertir_a_centigrados(numero)
+    assert isinstance(resultado, float), "El resultado debe ser un numero tipo Float"
+    assert resultado == -20.56, "El resultado no es el esperado"
